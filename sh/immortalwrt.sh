d897805d914dd7a2054578a74cac097e3bb1d046
ChenChen <cc2286927@sina.cn>
2024-10-23 16:17:19 +0800

    Update immortalwrt.sh

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -47,6 +47,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
 #######################################################################################
+git_sparse_clone master https://github.com/xiangfeidexiaohuo/extra-ipk op-xunlei
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede ruby
